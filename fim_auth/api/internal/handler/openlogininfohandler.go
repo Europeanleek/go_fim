5c055b6226b40c3f01ceaa7b382d29e1d51e6026
liyinjie <603015744@qq.com>
2025-05-20 23:50:29 +0800

    5-20

---
+++ fim_auth/api/internal/handler/openlogininfohandler.go
@@ -0,0 +1,21 @@
+package handler
+
+import (
+	"net/http"
+
+	"fim_server/fim_auth/api/internal/logic"
+	"fim_server/fim_auth/api/internal/svc"
+	"github.com/zeromicro/go-zero/rest/httpx"
+)
+
+func open_login_infoHandler(svcCtx *svc.ServiceContext) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		l := logic.NewOpen_login_infoLogic(r.Context(), svcCtx)
+		resp, err := l.Open_login_info()
+		if err != nil {
+			httpx.ErrorCtx(r.Context(), w, err)
+		} else {
+			httpx.OkJsonCtx(r.Context(), w, resp)
+		}
+	}
+}
---
+++ fim_auth/api/internal/svc/servicecontext.go
@@ -0,0 +1,15 @@
+package svc
+
+import (
+	"fim_server/fim_auth/api/internal/config"
+)
+
+type ServiceContext struct {
+	Config config.Config
+}
+
+func NewServiceContext(c config.Config) *ServiceContext {
+	return &ServiceContext{
+		Config: c,
+	}
+}
